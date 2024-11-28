9f366ad9e4d50e1fe3564be44b078e2d79184686
YukihaYukishiro <appletea0514@gmail.com>
2024-11-28 13:12:39 +0900

    出席のアイコン変えた

--- extension/backgrounds/attendFromTop.js
+++ extension/backgrounds/attendFromTop.js
@@ -1,5 +1,7 @@
 function addAttendFromTop() {
 
+    
+
     const sections = document.querySelectorAll('.T_I_section');
     for (let section of sections) {
         const a = section.children[0];
@@ -10,19 +12,25 @@ function addAttendFromTop() {
         }
         //出席ボタンの追加
         //画像の追加
-        const attend_img = document.createElement("img");
-        attend_img.src = chrome.runtime.getURL("../images/attend.jpg");
-        attend_img.classList.add("attend_img");
+        const attend_button = document.createElement("i");
+        attend_button.classList.add("fas");
+        attend_button.classList.add("fa-user-plus");
+        attend_button.classList.add("attend_button");
         //画像サイズの調整
-        attend_img.style.width = "20px";
-        attend_img.style.height = "20px";
+        // attend_button.style.width = "25px";
+        // attend_button.style.height = "25px";
+        attend_button.style.fontSize = "16px";
+        attend_button.style.cursor = "pointer";
+        attend_button.style.color = "gray";
         //onclickイベントの追加
-        attend_img.setAttribute("onclick", `checkAttendEntry(${href});`)                                                           
+        attend_button.setAttribute("onclick", `checkAttendEntry(${href});`) 
+
+
     
         //ボタンの実装
         let div_right = document.createElement("div");
         div_right.classList.add("text-right");
-        div_right.appendChild(attend_img);
+        div_right.appendChild(attend_button);
         section.insertBefore(div_right, section.children[1]);
     }
 
