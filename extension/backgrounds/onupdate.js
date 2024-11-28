3c238f2f6a650e5e1db4547c8c144c71ea387982
YukihaYukishiro <appletea0514@gmail.com>
2024-11-28 13:43:05 +0900

    新規インストール向けの画面を追加

--- extension/backgrounds/onupdate.js
+++ extension/backgrounds/onupdate.js
@@ -1,9 +1,20 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    if (!(details.reason === "update" && details.previousVersion === "0.8")) {
-        
+    if (details.reason == "install") {
+        chrome.tabs.create({
+            url: "chrome-extension://" + chrome.runtime.id + "/html/newuser.html"
+        });
+    }
+
+    
+    if (!(details.reason === "update" && details.previousVersion === "0.9")) {
+
         return;
     }
+
     chrome.tabs.create({
         url: "chrome-extension://" + chrome.runtime.id + "/html/patch.html"
-      });
+    });
+
+
+
 });
