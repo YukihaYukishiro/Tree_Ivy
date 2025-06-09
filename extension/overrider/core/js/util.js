b2522d0f20a0e79de1597e9312e85ca5716a43e5
YukihaYukishiro <appletea0514@gmail.com>
2025-06-09 21:54:39 +0900

    エラー消し（今度こそね）

--- extension/overrider/core/js/util.js
+++ extension/overrider/core/js/util.js
@@ -21,7 +21,7 @@ function waitForElement(selector, timeout = 10000) {
     if (timeout > 0) {
       setTimeout(() => {
         observer.disconnect();
-        reject(`要素「${selector}」が${timeout}ms以内に見つかりませんでした`);
+        resolve(`要素「${selector}」が${timeout}ms以内に見つかりませんでした`);
       }, timeout);
     }
   });
@@ -47,7 +47,7 @@ function waitForElements(selector, timeout = 10000) {
     if (timeout > 0) {
       setTimeout(() => {  
         observer.disconnect();
-        reject(`要素「${selector}」が${timeout}ms以内に見つからなかったよ〜💦`);
+        resolve(`要素「${selector}」が${timeout}ms以内に見つからなかったよ〜💦`);
       }, timeout);
     }
   });
